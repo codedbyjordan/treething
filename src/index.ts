7130852b4b90946394b6ddbc06c87257ff4d7d6c
Jordan Baron <jordannbaron@gmail.com>
2023-09-17 00:36:32 -0400

    example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import { parse } from "@babel/parser";
+
+const code = await Bun.file("src/parseMe.ts").text();
+
+const ast = parse(code);
+
+console.log(ast);
---
+++ src/parseMe.ts
@@ -0,0 +1,5 @@
+function callMe() {
+    return "heyyyy";
+}
+
+callMe();
